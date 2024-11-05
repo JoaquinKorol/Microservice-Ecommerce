bd5bd20d318cf499ac4729b25129bb2c93ae1fd2
JoaquinKorol <joaquinkorol@gmail.com>
2024-11-05 02:27:18 -0300

    Add UpdateDTo Validator

---
+++ UserServices/Validators/UpdateDTOValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using UserServices.DTOs;
+
+namespace UserServices.Validators
+{
+    public class UpdateDTOValidator : AbstractValidator<UpdateDTO>
+    {
+        public UpdateDTOValidator() 
+        { 
+            RuleFor(x => x.Email)
+                .EmailAddress()
+                .WithMessage("The email address is not valid.");
+
+        }
+    }
+}
