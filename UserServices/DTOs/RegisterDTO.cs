9d8cdc002354ad382b9e2f591b6cb961b4aededd
JoaquinKorol <joaquinkorol@gmail.com>
2024-11-05 02:13:43 -0300

    Add RegisterDTO

--- UserServices/DTOs/RegisterDTO.cs
+++ UserServices/DTOs/RegisterDTO.cs
@@ -6,7 +6,6 @@ namespace UserServices.DTOs
     {
         public string Name { get; set; }
         public string Email { get; set; }
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$", ErrorMessage = "The password must be at least 8 characters long and include uppercase letters, lowercase letters, and numbers.")]
         public string Password { get; set; }
     }
 }
