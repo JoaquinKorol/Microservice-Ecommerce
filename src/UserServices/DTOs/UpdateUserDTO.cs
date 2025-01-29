ca1be54d768a7f96fc23bb2cd1d45fd482a97106
JoaquinKorol <joaquinkorol@gmail.com>
2025-01-28 23:57:51 -0300

    Renamed UpdateDto to UpdateUserDto for clarity

---
+++ src/UserServices/DTOs/UpdateUserDTO.cs
@@ -0,0 +1,10 @@
+﻿using UserServices.Models;
+
+namespace UserServices.DTOs
+{
+    public class UpdateUserDTO
+    {
+        public string? Name { get; set; } 
+        public string? Email {  get; set; }
+    }
+}
