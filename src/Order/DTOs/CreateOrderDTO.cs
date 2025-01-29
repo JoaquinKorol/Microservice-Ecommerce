8b7d3a005a1858f01f65ae2c2bf52440fa7b0fa8
JoaquinKorol <joaquinkorol@gmail.com>
2025-01-29 04:51:50 -0300

    Remove Total Amount from DTO

--- src/Order/DTOs/CreateOrderDTO.cs
+++ src/Order/DTOs/CreateOrderDTO.cs
@@ -2,20 +2,14 @@
 {
     public class CreateOrderDTO
     {
-        // ID del usuario que está creando la orden
+       
         public int UserId { get; set; }
 
-        // Dirección de envío
+ 
         public string ShippingAddress { get; set; }
         
- 
-        // Monto total de la orden
-        public decimal TotalAmount { get; set; }
 
-        // Estado de la orden (esto puede ser un valor numérico que se relacione con una tabla de estados)
-        public int Status { get; set; }
-
-        // Lista de productos en la orden
+  
         public List<OrderItemDTO> OrderItems { get; set; } = new List<OrderItemDTO>();
     }
 
