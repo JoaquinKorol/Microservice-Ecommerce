e93836ea1486c833872c6195d061da5b8a314723
JoaquinKorol <joaquinkorol@gmail.com>
2025-01-30 18:06:01 -0300

    Create PaymentRequestDTO

---
+++ src/Payment/DTOs/PaymentRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Payment.DTOs
+{
+    public class PaymentRequestDTO
+    {
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+
+        public int OrderId { get; set; }
+    }
+}
