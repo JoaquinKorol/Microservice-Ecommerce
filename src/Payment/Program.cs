2842d9f468c21b1c16224ff6fa97ed2882d06383
JoaquinKorol <joaquinkorol@gmail.com>
2025-01-30 18:02:54 -0300

    Add HttpClient Service

--- src/Payment/Program.cs
+++ src/Payment/Program.cs
@@ -1,4 +1,12 @@
 
+using Core.Interfaces;
+using MercadoPago.Config;
+using Microsoft.EntityFrameworkCore;
+using Payment.Data;
+using Payment.Models;
+using Payment.Repositories;
+using Payment.Services;
+
 namespace Payment
 {
     public class Program
@@ -13,6 +21,15 @@ namespace Payment
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<PaymentDbContext>(options =>
+             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+           MercadoPagoConfig.AccessToken = builder.Configuration["MercadoPago:AccessToken"];
+
+            builder.Services.AddScoped<IRepository<Payments>, PaymentRepository>();
+            builder.Services.AddScoped<PaymentService>();
+            builder.Services.AddHttpClient();
 
             var app = builder.Build();
 
