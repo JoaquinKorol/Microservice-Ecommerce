7f486fd82ca00f9da36efe5259534787e1e4a2cb
JoaquinKorol <joaquinkorol@gmail.com>
2025-01-30 18:06:16 -0300

    Create PaymentDBContext

---
+++ src/Payment/Data/PaymentDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Payment.Data
+{
+    public class PaymentDbContext : DbContext
+    {
+        public PaymentDbContext(DbContextOptions<PaymentDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Payment.Models.Payments> Payments { get; set; }
+    }
+}
