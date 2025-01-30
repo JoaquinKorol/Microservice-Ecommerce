3ac92e7cf2eb92fe70156cc7c6f6e5285a80ee1a
JoaquinKorol <joaquinkorol@gmail.com>
2025-01-30 18:02:20 -0300

    Add GetPaymentById Endpoint

--- src/Payment/Controllers/PaymentController.cs
+++ src/Payment/Controllers/PaymentController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Core.Exceptions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Payment.DTOs;
+using Payment.Models;
+using Payment.Services;
 
 namespace Payment.Controllers
 {
@@ -7,10 +11,37 @@ namespace Payment.Controllers
     [ApiController]
     public class PaymentController : ControllerBase
     {
-        [HttpGet]
-        public string Payment()
+        private readonly PaymentService _paymentService;
+        public PaymentController(PaymentService paymentService)
         {
-            return "Servicio de pagos";
+            _paymentService = paymentService;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreatePayment([FromBody] PaymentRequestDTO request)
+        {
+            var paymentUrl = await _paymentService.CreatePaymentAsync(request.Amount, request.Description, request.OrderId);
+            return Ok(new { Url = paymentUrl }); 
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Payments>> GetPayments()
+        {
+            return await _paymentService.GetPaymentsAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Payments>> GetPaymentById(int id)
+        {
+            try
+            {
+                var payment = await _paymentService.GetPaymentByIdAsync(id);
+                return Ok(payment);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
         }
     }
 }
